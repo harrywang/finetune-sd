546ace6313a3c57aae24fac7d19d27deef548aed
Harry Wang <harryjwang@gmail.com>
2023-02-01 21:56:10 -0500

    added another dataset

--- generate-dogs.py
+++ generate-dogs.py
@@ -3,20 +3,27 @@ model_path = './outputs/dreambooth/dog'
 import torch
 from diffusers import StableDiffusionPipeline, DPMSolverMultistepScheduler
 
-pipe = StableDiffusionPipeline.from_pretrained('runwayml/stable-diffusion-v1-5', torch_dtype=torch.float16)
+if torch.cuda.is_available():
+    device = "cuda"
+    pipe = StableDiffusionPipeline.from_pretrained('runwayml/stable-diffusion-v1-5', torch_dtype=torch.float16)
+else:
+    device = "cpu"
+    pipe = StableDiffusionPipeline.from_pretrained('runwayml/stable-diffusion-v1-5', torch_dtype=torch.float32)
+print(device)
+
 pipe.scheduler = DPMSolverMultistepScheduler.from_config(pipe.scheduler.config)
-
 pipe.unet.load_attn_procs(model_path)
-pipe.to("cuda")
+pipe.to(device)
 
+output_folder = "test-img/"
 image = pipe("A photo of sks dog swimming", num_inference_steps=25).images[0]
-image.save("dog_swimming.png")
+image.save(output_folder + "dog_swimming.png")
 
 image = pipe("A photo of sks dog in a bucket", num_inference_steps=25).images[0]
-image.save("dog_bucket.png")
+image.save(output_folder + "dog_bucket.png")
 
 image = pipe("A photo of sks dog in front of eiffel tower", num_inference_steps=25).images[0]
-image.save("dog_tower.png")
+image.save(output_folder + "dog_tower.png")
 
 image = pipe("A photo of sks dog on the great wall", num_inference_steps=25).images[0]
-image.save("dog_wall.png")
+image.save(output_folder + "dog_wall.png")
---
+++ test-img/README.md
@@ -0,0 +1,1 @@
+test generated images are here
